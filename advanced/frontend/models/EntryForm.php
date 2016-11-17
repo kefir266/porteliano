dafc9d07211d26ad3b9d1caea67c40fdd4dbc291
Dmytro Gorbatko <kefir266@gmail.com>
2016-11-17 10:57:17 +0300

    Uploading

---
+++ advanced/frontend/models/EntryForm.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hitch
+ * Date: 10.11.2016
+ * Time: 15:56
+ */
+namespace app\models;
+
+use yii\base\Model;
+
+class EntryForm extends Model
+{
+    public $name;
+    public $email;
+
+    public function rules()
+    {
+        return [
+            [['name', 'email'], 'required'],
+            ['email', 'email'],
+        ];
+    }
+}
