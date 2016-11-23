df19438b418e7cc20872fe2a1b5af83168770eb3
Antis <antis.smith@gmail.com>
2016-11-23 15:19:34 +0300

    позиционирование плиток в новинки

--- advanced/frontend/views/site/home/block_04-novelty.php
+++ advanced/frontend/views/site/home/block_04-novelty.php
@@ -16,7 +16,20 @@ use app\assets\MainAsset;
 AppAsset::register($this);
 MainAsset::register($this);
 
+
+// псевдоним пути к папке на основе другого псевдонима
+Yii::setAlias('@novelty', '@web/img/novelty/');
 ?>
 <div class="wrap-novelty">
-    
+    <div class="novelty">
+        <?=Html::tag('h3','Новинки');?>
+            <?=Html::img('@novelty/door_1.jpg',
+                        ['alt'=>'door_1', 'class' => 'tile']);?>
+            <?=Html::img('@novelty/door_2.jpg',
+                        ['alt'=>'door_1', 'class' => 'tile']);?>
+            <?=Html::img('@novelty/door_3.jpg',
+                        ['alt'=>'door_1', 'class' => 'tile']);?>
+            <?=Html::img('@novelty/door_4.jpg',
+                        ['alt'=>'door_1', 'class' => 'tile']);?>
+    </div>
 </div>
