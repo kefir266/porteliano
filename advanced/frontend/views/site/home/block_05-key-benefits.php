8367ec027360fe031c3e3e490b400f42372877da
Antis <antis.smith@gmail.com>
2016-11-24 11:18:42 +0300

    добавлен  иконки в файлы block_05-key-benefits

--- advanced/frontend/views/site/home/block_05-key-benefits.php
+++ advanced/frontend/views/site/home/block_05-key-benefits.php
@@ -17,5 +17,13 @@ MainAsset::register($this);
 
 ?>
 <div class="wrap-key-benefits">
-    
+    <div class="panel-key-benefits">
+        <?= Html::tag('h2','Ключевые преимущества');?>
+        <span class="glyphicon glyphicon-time"></span>
+        <span class="glyphicon glyphicon-star"></span>
+        <span class="glyphicon glyphicon-alert"></span>
+        <span class="glyphicon glyphicon-scale"></span>
+        <span class="glyphicon glyphicon-calc"></span>
+        <span class="glyphicon glyphicon-ruby"></span>
+    </div>
 </div>
