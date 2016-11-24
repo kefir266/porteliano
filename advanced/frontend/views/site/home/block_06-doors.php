a8e63cbbfef7fda6e848625a9bf731d0337f4a2d
Antis <antis.smith@gmail.com>
2016-11-24 20:13:39 +0300

    fix выравнивание картинок

--- advanced/frontend/views/site/home/block_06-doors.php
+++ advanced/frontend/views/site/home/block_06-doors.php
@@ -18,76 +18,119 @@ MainAsset::register($this);
 
 
 // псевдоним пути к папке на основе другого псевдонима
-Yii::setAlias('@novelty', '@web/img/novelty/');
+Yii::setAlias('@doors', '@web/img/doors/');
 
-$category = ['Межкомнатная дверь', 'Межкомнатная перегородка'];
-$manufacturer = ['Ghizzi&Bennatti','New Design porte','FOA','Bluinterni'];
-$model = ['Tekna 2', 'Giudetto IMP 1011 QQ H', 'Bell neutro', 'MULTY'];
-$price = ['720', '688', '1265', '750'];
+// TODO заменить на загрузку из базы
+$category = ['Входная дверь'];
+$doorData_2 = ['Bauxt','Bauxt','Security','Bauxt'];
+$doorData_3 = ['Export 1106', 'Export 1136', 'SECURITY', 'Elite 1115'];
+$price = ['1545', '1545', '2119', '2194'];
 
 // массив для заполнения информационных полей под плитками новинок
-// TODO заменить на загрузку из базы
+
 $info = [
     [
         0 => $category[0],
-        1 => $manufacturer[0],
-        2 => $model[0],
+        1 => $doorData_2[0],
+        2 => $doorData_3[0],
         3 => $price[0],
     ],
     [
         0 => $category[0],
-        1 => $manufacturer[1],
-        2 => $model[1],
+        1 => $doorData_2[1],
+        2 => $doorData_3[1],
         3 => $price[1],
     ],
     [
-        0 => $category[1],
-        1 => $manufacturer[2],
-        2 => $model[2],
+        0 => $category[0],
+        1 => $doorData_2[2],
+        2 => $doorData_3[2],
         3 => $price[2],
     ],
     [
         0 => $category[0],
-        1 => $manufacturer[3],
-        2 => $model[3],
+        1 => $doorData_2[3],
+        2 => $doorData_3[3],
         3 => $price[3],
     ]
 ]
 ?>
 <div class="wrap-doors">
-    <div class="novelty">
-        <div class="wrap-tiles">
-            <?php
-            echo Html::tag('h2','Новинки');
-            echo Html::img('@novelty/door_1.jpg',
-                ['alt'=>'door_1', 'class' => 'tile']);
-            echo Html::img('@novelty/door_2.jpg',
-                ['alt'=>'door_1', 'class' => 'tile']);
-            echo Html::img('@novelty/door_3.jpg',
-                ['alt'=>'door_1', 'class' => 'tile']);
-            echo Html::img('@novelty/door_4.jpg',
-                ['alt'=>'door_1', 'class' => 'tile']);
-            ?>
-        </div>
-        <div class="wrap-info">
-            <?php
-            for ($i = 0; $i < 4; $i++){
-                echo Html::beginTag('div', ['class' => 'info']);
-                for ($j = 0; $j < 3; $j++) {
-                    echo Html::tag('p', $info[$i][$j]);
-                }
-                echo Html::tag('div','',['class' => 'delimiter']);
-                echo Html::beginTag('div',['class' => 'block-4-price']);
-                echo Html::tag('div','€ '.$info[$i][3],['class' => 'block-4-price-count']);
-                echo Html::tag('div','',['class' => 'glyphicon glyphicon-heart-empty ']);
-                echo Html::endTag('div');
-                echo Html::endTag('div');
-            }//€
-            ?>
+    <!-- Навигация -->
+    <ul class="nav nav-tabs nav-pills " role="tablist">
+        <li class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">МЕЖКОМНАТНЫЕ</a></li>
+        <li><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">ВХОДНЫЕ</a></li>
+    </ul>
+    <!-- Содержимое вкладок -->
+    <div class="tab-content">
+        <div role="tabpanel" class="tab-pane active" id="home">
+            <div class="novelty doors-panel">
+                <div class="wrap-tiles">
+                    <?php
+                    echo Html::tag('h2','Двери');
+                    for ($i = 5; $i < 9; $i++){
+                        echo Html::beginTag('div', ['class' => 'tile']);
+                        echo Html::img('@doors/door_'.$i.'.jpg',
+                            ['alt'=>'door_'.$i, 'class' => '']);
+                        echo Html::endTag('div');
+                    }
+                    ?>
+                </div>
+                <div class="wrap-info">
+                    <?php
+                    for ($i = 0; $i < 4; $i++){
+                        echo Html::beginTag('div', ['class' => 'info']);
+                        for ($j = 0; $j < 3; $j++) {
+                            echo Html::tag('p', $info[$i][$j]);
+                        }
+                        echo Html::tag('div','',['class' => 'delimiter']);
+                        echo Html::beginTag('div',['class' => 'block-4-price']);
+                        echo Html::tag('div','€ '.$info[$i][3],['class' => 'block-4-price-count']);
+                        echo Html::tag('div','',['class' => 'glyphicon glyphicon-heart-empty ']);
+                        echo Html::endTag('div');
+                        echo Html::endTag('div');
+                    }//€
+                    ?>
 
+                </div>
+                <button type="button" class="btn ">ПОКАЗАТЬ БОЛЬШЕ</button>
+            </div>
         </div>
-        <button type="button" class="btn ">ПОКАЗАТЬ БОЛЬШЕ</button>
+        <div role="tabpanel" class="tab-pane" id="profile">
+            <div class="novelty doors-panel">
+                <div class="wrap-tiles">
+                    <?php
+                    echo Html::tag('h2','Двери');
+                    for ($i = 1; $i < 5; $i++){
+                        echo Html::beginTag('div', ['class' => 'tile']);
+                        echo Html::img('@novelty/door_'.$i.'.jpg',
+                            ['alt'=>'door_'.$i, 'class' => '']);
+                        echo Html::endTag('div');
+                    }
+                    ?>
+                </div>
+                <div class="wrap-info">
+                    <?php
+                    for ($i = 0; $i < 4; $i++){
+                        echo Html::beginTag('div', ['class' => 'info']);
+                        for ($j = 0; $j < 3; $j++) {
+                            echo Html::tag('p', $info[$i][$j]);
+                        }
+                        echo Html::tag('div','',['class' => 'delimiter']);
+                        echo Html::beginTag('div',['class' => 'block-4-price']);
+                        echo Html::tag('div','€ '.$info[$i][3],['class' => 'block-4-price-count']);
+                        echo Html::tag('div','',['class' => 'glyphicon glyphicon-heart-empty ']);
+                        echo Html::endTag('div');
+                        echo Html::endTag('div');
+                    }//€
+                    ?>
+
+                </div>
+                <button type="button" class="btn ">ПОКАЗАТЬ БОЛЬШЕ</button>
+            </div>
+        </div>
     </div>
+
     <div class="arrow"></div>
 </div>
 
