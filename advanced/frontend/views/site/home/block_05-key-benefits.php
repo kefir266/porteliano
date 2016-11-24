aa24a16f5809c917aa44dc063514206b4f364af0
Antis <antis.smith@gmail.com>
2016-11-24 11:46:28 +0300

    добавлено центрирование иконок в круге block_05.css

--- advanced/frontend/views/site/home/block_05-key-benefits.php
+++ advanced/frontend/views/site/home/block_05-key-benefits.php
@@ -20,10 +20,11 @@ MainAsset::register($this);
     <div class="panel-key-benefits">
         <?= Html::tag('h2','Ключевые преимущества');?>
         <span class="glyphicon glyphicon-time"></span>
-        <span class="glyphicon glyphicon-star"></span>
-        <span class="glyphicon glyphicon-alert"></span>
-        <span class="glyphicon glyphicon-scale"></span>
-        <span class="glyphicon glyphicon-calc"></span>
         <span class="glyphicon glyphicon-ruby"></span>
+        <span class="glyphicon glyphicon-star"></span>
+        <span class="glyphicon glyphicon-scale"></span>
+        <span class="glyphicon glyphicon-alert"></span>
+        <span class="glyphicon glyphicon-calc"></span>
+
     </div>
 </div>
