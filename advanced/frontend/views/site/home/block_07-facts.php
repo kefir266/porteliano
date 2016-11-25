f366122af91566cc6c015c63d45c043c3e547c58
Antis <antis.smith@gmail.com>
2016-11-25 16:23:57 +0300

    add факты в цифрах

--- advanced/frontend/views/site/home/block_07-facts.php
+++ advanced/frontend/views/site/home/block_07-facts.php
@@ -21,6 +21,26 @@ MainAsset::register($this);
     <div class="img-holder"
          data-image="<?='img/background/facts.jpg'?>">
     </div>
+    <div class="facts">
+        <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-3">
+                <h1>20 лет</h1>
+                <p>На  российском рынке</p>
+            </div>
+            <div class="col-md-1"></div>
+            <div class="col-md-3">
+                <h1>6 000</h1>
+                <p>Довольных клиентов</p>
+            </div>
+            <div class="col-md-4">
+            <h1>200 000</h1>
+            <p>Созданных дверей
+                и перегородок</p>
+        </div>
+
+        </div>
+    </div>
 
     <script>
         $('div.wrap-facts-at-Glance > .img-holder').imageScroll({
