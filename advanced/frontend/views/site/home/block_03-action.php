3c781b25264150fb7a99a60c37195d27f0834908
Antis <antis.smith@gmail.com>
2016-11-25 11:27:43 +0300

    feature(Акция) насторил праллакс

--- advanced/frontend/views/site/home/block_03-action.php
+++ advanced/frontend/views/site/home/block_03-action.php
@@ -13,15 +13,27 @@ use app\assets\MainAsset;
 
 AppAsset::register($this);
 MainAsset::register($this);
-
-
 ?>
-<div class="wrap-action">
-    <!--<div class="parallax-window" data-parallax="scroll" data-image-src="../images/blog.jpg"></div>-->
+<div  class="wrap-action">
+    <div class="img-holder"
+         data-image="<?='img/background/FOTO_INTRO_01.jpg'?>">
+    </div>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://rawgithub.com/pederan/Parallax-ImageScroll/master/jquery.imageScroll.min.js"></script>
 
-    <p style="
-    font-size:4em;
-    text-align: center;
-    line-height: 647px;
-    ">Тут Будет блок с параллаксом</p>
+    <script>
+
+        $('.img-holder').imageScroll({
+            holderClass: 'imageHolder',
+            container: $('div.wrap-action'),
+            speed: 0.2,
+            coverRatio: 0.75,
+            mediaWidth: 2000,
+            mediaHeight: 1415,
+            holderMinHeight: 1000,
+            parallax: true,
+            touch: false
+        });
+
+    </script>
 </div>
