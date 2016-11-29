b6b16b605878eb6b4aa8b2d75b6c9d7932a67f36
Antis <antis.smith@gmail.com>
2016-11-29 17:12:00 +0300

    fix(09_O kompanii) параллакс

--- advanced/frontend/assets/PagesAsset.php
+++ advanced/frontend/assets/PagesAsset.php
@@ -14,14 +14,14 @@ class PagesAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
-    
+
     public $css = [
         'css/pages/doors.css',
         'css/pages/septa.css',
         'css/pages/manuf.css',
         'css/pages/manuf-inner.css',
         'css/pages/about.css',
-        'css/main/block_3.css',
+
     ];
     public $js = [
         // доп. библиотеки
