1a6c2f42b0d48db9852e47b062bf44f181c49ed0
Antis <antis.smith@gmail.com>
2016-11-30 14:49:26 +0300

    fix(03_doors_catalog) выравнивание плиток дверей

--- advanced/frontend/views/pages/03_Dveri_katalog.php
+++ advanced/frontend/views/pages/03_Dveri_katalog.php
@@ -34,6 +34,51 @@ $title = $products['section']['title'];
 foreach ($sections->getMenu() as $section) {
     $items[] = $section;
 }
+
+
+// псевдоним пути к папке на основе другого псевдонима
+Yii::setAlias('@doors', '@web/img/doors');
+
+// TODO заменить на загрузку из базы
+$category = ['Входная дверь'];
+$doorData_2 = ['Bauxt', 'Bauxt', 'Security', 'Bauxt'];
+$doorData_3 = ['Export 1106', 'Export 1136', 'SECURITY', 'Elite 1115'];
+$price = ['1545', '1545', '2119', '2194'];
+
+// массив для заполнения информационных полей под плитками новинок
+
+$info = [
+    [
+        0 => $category[0],
+        1 => $doorData_2[0],
+        2 => $doorData_3[0],
+        3 => $price[0],
+    ],
+    [
+        0 => $category[0],
+        1 => $doorData_2[1],
+        2 => $doorData_3[1],
+        3 => $price[1],
+    ],
+    [
+        0 => $category[0],
+        1 => $doorData_2[2],
+        2 => $doorData_3[2],
+        3 => $price[2],
+    ],
+    [
+        0 => $category[0],
+        1 => $doorData_2[3],
+        2 => $doorData_3[3],
+        3 => $price[3],
+    ],
+    [
+        0 => $category[0],
+        1 => $doorData_2[3],
+        2 => $doorData_3[3],
+        3 => $price[3],
+    ]
+]
 ?>
 <div class="door-catalog">
     <div class="panel-quick-selection">
@@ -48,7 +93,7 @@ foreach ($sections->getMenu() as $section) {
             <div class="col-lg-12 ">
                 <div class="flex-container">
                     <div class="material">
-                        <h5>Материал</h5>
+                        <h5>Вид</h5>
                         <?php
                         echo ButtonDropdown::widget([
                             'options' => ['class' => 'btn-default'],
@@ -124,6 +169,45 @@ foreach ($sections->getMenu() as $section) {
                         ]);
                         ?>
                     </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-5">
+                <span>Сортировать по:</span>
+                <span class="btn btn-link">Алфавиту</span>
+                <span class="btn btn-link">Цене</span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="sampling-area">
+                    <?php
+                    for ($k = 0; $k < 3; $k++) {
+                        //добавляет карточки в область прокрутки $i -№ дверей
+                        for ($i = 0; $i < 5; $i++) {
+
+                            //вывод картинок
+                            echo Html::beginTag('li', ['class' => 'tile']);
+                            //  TODO ($i+5) для теста, поставить $i
+                            echo Html::img('@doors/door_' . ($i + 5) . '.PNG',
+                                ['alt' => 'door_' . ($i + 5), 'class' => '']);
+
+                            //заполняет карточку $i- № дверей, j- строка карточки
+                            echo Html::beginTag('div', ['class' => 'info']);
+                            for ($j = 0; $j < 3; $j++) {
+                                echo Html::tag('p', $info[$i][$j]);
+                            }
+                            echo Html::tag('div', '', ['class' => 'delimiter']);
+                            echo Html::beginTag('div', ['class' => 'block-4-price']);
+                            echo Html::tag('div', '€ ' . $info[0][3], ['class' => 'block-4-price-count']);
+                            echo Html::tag('div', '', ['class' => 'glyphicon glyphicon-heart-empty ']);
+                            echo Html::endTag('div');
+                            echo Html::endTag('div');
+                            echo Html::endTag('li');
+                        }
+                    }
+                    ?>
                 </div>
             </div>
         </div>
