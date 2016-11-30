999721aa6b73ade890896334b4cdcc42fcaf4736
Antis <antis.smith@gmail.com>
2016-11-30 21:37:03 +0300

    fix(doors) надписи

--- advanced/frontend/views/pages/02_Dveri.php
+++ advanced/frontend/views/pages/02_Dveri.php
@@ -25,7 +25,7 @@ $this->params['breadcrumbs'][] = [
     'template' => "<li> {link} </li>\n", // template for this link only
 
 ];
-
+$coverTitles = ['деревянные','Стеклянные','Радиусные','Дверисо стеклом','Металлические двери','деревянные двери'];
 $bottomHead = 'Элитные итальянские двери, межкомнатные и входные';
 $bottomContent_1 = 'Архитектура, живопись, дизайн интерьера, мебель и, наконец, двери — Италия по праву носит титул страны, в которой рождается модерн, со временем переходящий в классику. Компания "Porteliano" работает для того, чтобы Вы могли перенести итальянскую традицию в Ваш дом или офис.';
 $bottomContent_2 = 'Речь идет о входных конструкциях непревзойденного качества и восхитительного дизайна, которые мы готовы предложить нашим покупателям. Благодаря большому количеству компаний-производителей, с которыми у нас давно налажено тесное сотрудничество, Вы можете выбирать двери итальянские из широчайшего ассортимента.';
@@ -40,10 +40,12 @@ $bottomContent_2 = 'Речь идет о входных конструкциях
         <div class="row">
             <?php
             for ($i = 1; $i < 5; $i++) {
-                echo '<div class="col-md-3">';
+                echo '<div class="col-md-3 plate-inn-doors">';
                 echo Html::beginTag('a',['href' => Url::to(['pages/doorcatalog', 'ind' => '0'])]);
                 echo Html::img("@doors/inner_0$i.jpg",
                     ['alt' => "door_0$i", 'class' => 'tile']);
+                echo '<div class="doors-gradient doors-inn-gradient-pos"></div>
+                        <h2 class="center-block">'.$coverTitles[$i-1].'</h2>';
                 echo Html::endTag('a');
                 echo '</div>';
             }
@@ -57,10 +59,12 @@ $bottomContent_2 = 'Речь идет о входных конструкциях
         <div class="row">
             <?php
             for ($i = 1; $i < 3; $i++) {
-                echo '<div class="col-md-6">';
+                echo '<div class="col-md-6 plate-out-doors">';
                 echo Html::beginTag('a',['href' => Url::to(['pages/doorcatalog', 'ind' => '1'])]);
                 echo Html::img("@doors/outer_0$i.jpg",
                     ['alt' => "door_0$i", 'class' => 'tile']);
+                echo '<div class="doors-gradient doors-out-gradient-pos"></div>
+                        <h2 class="center-block">'.$coverTitles[$i+3].'</h2>';
                 echo Html::endTag('a');
                 echo '</div>';
             }
@@ -72,14 +76,18 @@ $bottomContent_2 = 'Речь идет о входных конструкциях
             </div>
         </div>
         <div class="row">
-            <?php
-            echo '<div class="col-md-6 wrap-doors-tiles">';
-            echo Html::beginTag('a',['href' => Url::to(['pages/doorcatalog', 'ind' => '2'])]);
-            echo Html::img("@doors/grips.jpg",
-                ['alt' => "door_0$i", 'class' => 'tile']);
-            echo '</div>';
-            echo Html::endTag('a');
-            ?>
+            
+            <div class="col-md-6 wrap-doors-tiles plate">
+            <?= Html::beginTag('a',['href' => Url::to(['pages/doorcatalog', 'ind' => '2'])]);?>
+            <?= Html::img("@doors/grips.jpg",
+                ['alt' => "door_0$i", 'class' => 'tile']);?>
+            
+            <div class="doors-gradient grips-gradient-pos"></div>
+            <h2 class="center-block">для межкомнатных дверей
+            </h2>';
+            <?= Html::endTag('a');?>
+            </div>
+
         </div>
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
