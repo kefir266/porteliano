7039c1f8ce786b9366cd99f9449d54b4895711c7
Antis <antis.smith@gmail.com>
2016-12-01 11:27:02 +0300

    fix(block_08-septa) прокрутка перегородок

--- advanced/frontend/views/site/home/block_08-septa.php
+++ advanced/frontend/views/site/home/block_08-septa.php
@@ -48,45 +48,79 @@ $info = [
         1 => $doorData_2[3],
         2 => $doorData_3[3],
         3 => $price[3],
-    ]
-]
+    ],
+    [
+        0 => $category[0],
+        1 => $doorData_2[3],
+        2 => $doorData_3[3],
+        3 => $price[3],
+    ],
+    [
+        0 => $category[0],
+        1 => $doorData_2[3],
+        2 => $doorData_3[3],
+        3 => $price[3],
+    ],
+    [
+        0 => $category[0],
+        1 => $doorData_2[3],
+        2 => $doorData_3[3],
+        3 => $price[3],
+    ],
+    [
+        0 => $category[0],
+        1 => $doorData_2[3],
+        2 => $doorData_3[3],
+        3 => $price[3],
+    ],
+
+];
+//echo Html::img('@septa/septa_'.$i.'.jpg',
 ?>
 <article id="septa" class="wrap-septa" data-item="a3">
-    <div class="novelty doors-panel">
-        <div class="wrap-tiles">
-            <?php
-            echo Html::tag('h2','Перегородки');
-            for ($i = 1; $i < 5; $i++){
-                echo Html::beginTag('div', ['class' => 'tile']);
-                echo Html::img('@septa/septa_'.$i.'.jpg',
-                    ['alt'=>'door_'.$i, 'class' => '']);
-                echo Html::endTag('div');
-            }
-            ?>
+    <div class="row">
+        <div class="col-md-offset-5">
+            <?=Html::tag('h2','Перегородки');?>
         </div>
-        <div class="wrap-info">
-            <?php
-            for ($i = 0; $i < 4; $i++){
-                echo Html::beginTag('div', ['class' => 'info']);
-                for ($j = 0; $j < 3; $j++) {
-                    echo Html::tag('p', $info[$i][$j]);
-                }
-                echo Html::tag('div','',['class' => 'delimiter']);
-                echo Html::beginTag('div',['class' => 'block-4-price']);
-                echo Html::tag('div','€ '.$info[$i][3],['class' => 'block-4-price-count']);
-                echo Html::tag('div','',['class' => 'glyphicon glyphicon-heart-empty ']);
-                echo Html::endTag('div');
-                echo Html::endTag('div');
-            }//€
-            ?>
+    </div>
+    <div class="row">
+        <div class="col-md-offset-5">
+            <div class="novelty doors-panel septa-panel running-ribbon-septa">
+                <div class="wrap-tiles view">
+                    <ul>
+                        <?php
+                        //добавляет карточки в область прокрутки $i -№ дверей
+                        for ($i = 0; $i < 8; $i++) {
 
+                            //вывод картинок
+                            echo Html::beginTag('li', ['class' => 'tile']);
+                            //  TODO ($i+1) для теста, поставить $i
+                            echo Html::img('@septa/septa_'.($i+1).'.jpg',
+                                ['alt' => 'door_' . ($i), 'class' => '']);
+
+                            //заполняет карточку $i- № дверей, j- строка карточки
+                            echo Html::beginTag('div', ['class' => 'info']);
+                            for ($j = 0; $j < 3; $j++) {
+                                echo Html::tag('p', $info[$i][$j]);
+                            }
+                            echo Html::tag('div', '', ['class' => 'delimiter']);
+                            echo Html::beginTag('div', ['class' => 'block-4-price']);
+                            echo Html::tag('div', '€ ' . $info[0][3], ['class' => 'block-4-price-count']);
+                            echo Html::tag('div', '', ['class' => 'glyphicon glyphicon-heart-empty ']);
+                            echo Html::endTag('div');
+                            echo Html::endTag('div');
+                            echo Html::endTag('li');
+                        }
+                        ?>
+                    </ul>
+                </div>
+                <div id="show" >
+                    <button id="prev" class="btn btn-link" data-param="prev"></button>
+                    <button id="next" class="btn btn-link" data-param="next"></button>
+                </div>
+                <a href="<?=Url::to(['pages/septa']); ?>" class="btn btn-default btn-lg" role="button">ПОКАЗАТЬ БОЛЬШЕ</a>
+            </div>
         </div>
-        <!--TODO: сделать прокрутку при нажатии на стрелки -->
-        <div id="show" >
-            <button id="prev" class="btn btn-link" data-param="prev"></button>
-            <button id="next" class="btn btn-link" data-param="next"></button>
-        </div>
-        <a href="<?=Url::to(['pages/septa']); ?>" class="btn btn-default btn-lg" role="button">ПОКАЗАТЬ БОЛЬШЕ</a>
     </div>
     <div class="arrow"></div>
 </article>
