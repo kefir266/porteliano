1c393926225b713cccd512c11b56c0dc73ea22df
Antis <antis.smith@gmail.com>
2016-12-04 11:51:43 +0300

    chore(ribbon) выравнивание кода, исправление коментариев

--- advanced/frontend/views/layouts/ribbonElement.php
+++ advanced/frontend/views/layouts/ribbonElement.php
@@ -5,45 +5,45 @@
  * Date: 02.12.2016
  * Time: 12:02
  */
-
+/** @var frontend\models\Product $products */
 use yii\helpers\Html;
 use yii\helpers\Url;
 
 
-                    //добавляет карточки в область прокрутки $i -№ дверей
-                    foreach ($products['products'] as $product){
-                        //вывод картинок
-                        echo Html::beginTag('div', ['class' => 'tile']);
-                        //  TODO ($i+5) для теста, поставить $i
-                        echo Html::img('@img/'.$product->manufacturer->title.'/'
-                            .$product->img,
-                            ['alt' => $product->title, 'class' => '']);
+//добавляются карточки в область прокрутки 
+foreach ($products['products'] as $product) {
+    //вывод картинок
+    echo Html::beginTag('div', ['class' => 'tile']);
 
-                        //заполняет карточку $i -№ дверей j- строка карточки
-                        echo Html::beginTag('div', ['class' => 'info']);
-                        echo Html::tag('p', $product->section->title);
-                        echo Html::tag('p', $product->manufacturer->title);
-                        echo Html::tag('p', $product->title);
-                        echo Html::tag('div', '', ['class' => 'delimiter']);
-                        echo Html::beginTag('div', ['class' => 'block-4-price']);
-                        $price = $product->prices;
-                        echo
-                        Html::tag('div',
-                            Html::tag('a','€ '
-                            .  (( count($price) > 0) ?
-                                current($price)->cost
-                                : ''), ['href' =>
-                                Url::to(['cart/add','id' => $product->id])]),
-                            ['class' => 'block-4-price-count add-to-cart',
-                                'id' => 'p'.$product->id]
-                        );
-                        echo Html::tag('a',
-                            Html::tag('div', '',['class' => 'glyphicon glyphicon-heart-empty add-to-wish ',
-                                'id' => 'w'.$product->id]),
-                            ['href' =>
-                                Url::to(['cart/addWish', 'id' => $product->id])]);
-                        echo Html::endTag('div');
-                        echo Html::endTag('div');
-                        echo Html::endTag('div');
-                    }
-                    ?>
+    echo Html::img('@img/' . $product->manufacturer->title . '/'
+        . $product->img,
+        ['alt' => $product->title, 'class' => '']);
+
+    //заполняется карточка
+    echo Html::beginTag('div', ['class' => 'info']);
+    echo Html::tag('p', $product->section->title);
+    echo Html::tag('p', $product->manufacturer->title);
+    echo Html::tag('p', $product->title);
+    echo Html::tag('div', '', ['class' => 'delimiter']);
+    echo Html::beginTag('div', ['class' => 'block-4-price']);
+    $price = $product->prices;
+    echo
+    Html::tag('div',
+        Html::tag('a', '€ '
+            . ((count($price) > 0) ?
+                current($price)->cost
+                : ''), ['href' =>
+            Url::to(['cart/add', 'id' => $product->id])]),
+        ['class' => 'block-4-price-count add-to-cart',
+            'id' => 'p' . $product->id]
+    );
+    echo Html::tag('a',
+        Html::tag('div', '', ['class' => 'glyphicon glyphicon-heart-empty add-to-wish ',
+            'id' => 'w' . $product->id]),
+        ['href' =>
+            Url::to(['cart/addWish', 'id' => $product->id])]);
+    echo Html::endTag('div');
+    echo Html::endTag('div');
+    echo Html::endTag('div');
+}
+?>
