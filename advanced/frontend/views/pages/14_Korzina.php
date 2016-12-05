1e8f0ebea8122bf6a7a088c537fd598bd3ec3fbf
Antis <antis.smith@gmail.com>
2016-12-05 12:12:54 +0300

    feature(wishlist) добавлена верстка для избранного
    адрес:
    porteliano/pages/wishlist

---
+++ advanced/frontend/views/pages/14_Korzina.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Antis
+ * Date: 05.12.2016
+ * Time: 09:05
+ */
+/* @var $this yii\web\View */
+
+/*  models  */
+
+/*  widgets  */
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+/*  assets  */
+use app\assets\BasketAsset;
+
+BasketAsset::register($this);
+Yii::setAlias('@img', '@web/img/');
+?>
+<div class="wrap-basket">
+    <!-- заголовок -->
+    <div class="row">
+        <div class="col-md-2 col-md-offset-3 ">
+            <h1>Ваша корзина</h1>
+        </div>
+        <div class="col-md-2 col-md-offset-3 ">
+            <h4 id="count-goods"><?= $countGoods ?></h4>
+        </div>
+    </div>
+    <!-- товары -->
+    <?php
+    // TODO: переделать на загрузку из базы
+    for ($i = 0; $i < 2; $i++) {
+        $countGoods = '2 товара';
+        $description = 'Межкомнатная дверь Impronta Mod. Over 1000';
+        $pathToIcon = '@img/Romagnoli/AC 1B.PNG';
+        echo '
+    <div class="row goods-row">
+        <!-- иконка -->
+        <div class="col-md-1 col-md-offset-3 ">
+            ' .
+            Html::img($pathToIcon, ['class' => 'door-icon','alt' => "door icon"])
+            . '
+        </div>
+        <!-- описание и удаление -->
+        <div class="col-md-4">
+            <!-- описание -->
+            <div class="row">
+                <div class="col-md-12">
+                    <span class="description">
+                    ' . $description . '
+                    </span>
+                </div>
+            </div>
+            <!-- удаление -->
+            <div class="row">
+                <div class="col-md-12">
+                    <a class="delete" href="#idToDelete">Удалить</a>
+                </div>
+            </div>
+        </div>
+        <!-- кнопка в корзину -->
+        <div class="col-md-1 button-area">
+            <a href="/#">
+                <span
+                    class="glyphicon glyphicon-shopping-cart btn btn-default basket-button"
+                    role="button"
+                ></span>
+            </a>
+        </div>
+    </div>
+    ';
+    }
+    ?>
+    <!-- разделитель -->
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3 ">
+            <hr/>
+        </div>
+    </div>
+</div>
+
+
+
