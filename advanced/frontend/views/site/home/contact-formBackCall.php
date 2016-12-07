2edf6608c315e970223c2ef918e3cbc0bb84f6de
Antis <antis.smith@gmail.com>
2016-12-07 16:37:34 +0300

    feature(form) форма для обратного звонка

---
+++ advanced/frontend/views/site/home/contact-formBackCall.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Antis
+ * Date: 27.11.2016
+ * Time: 1:29
+ */
+/* @var @questionForm \frontend\models\QuestionForm */
+
+/*  models  */
+use  frontend\models\QuestionForm;
+/*  widgets  */
+use yii\helpers\Html;
+//use yii\widgets\ActiveForm;
+use yii\bootstrap\ActiveForm;
+?>
+
+<div class="contact-form  contact-form-back-call">
+    <div class="contact-form-panel">
+        <div class="row">
+            <article class="col-lg-12">
+                <h2 class="contact-form-header">Контакты</h2>
+                <div class="contact-form-city">м. Ленинский проспект</div>
+                <div class="contact-form-str">3-й Донской проезд, дом 1</div>
+                <div class="contact-form-phone">+7(495) 742-17-24,  +7(495) 123-65-56</div>
+                <div class="contact-form-mail">absolute@ak-in.ru</div>
+                <hr style="
+                    border: none; /* Убираем границу для браузера Firefox */
+                    color: #9a2434; /* Цвет линии для остальных браузеров */
+                    background-color:#9a2434; /* Цвет линии для браузера Firefox и Opera */
+                    height: 2px; /* Толщина линии */
+                   "/>
+            </article>
+
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <?php
+                $form = ActiveForm::begin([
+                    'id' => 'login-form',
+                    'fieldConfig' => [
+                        'template' => "{label}\n{beginWrapper}\n{input}\n{hint}\n{error}\n{endWrapper}",
+
+                        'horizontalCssClasses' => [
+                            'label' => 'col-sm-4',
+                            'offset' => 'col-sm-offset-4',
+                            'wrapper' => 'col-sm-8',
+                            'error' => '',
+                            'hint' => '',
+                        ],
+
+                    ],
+                    'options' => ['class' => 'form-horizontal'],
+                ]); ?>
+                <?= $form->field($questionForm, 'username',
+                    [
+                        'horizontalCssClasses' => [
+                            'wrapper' => 'col-sm-2',
+                        ],
+                        'template' => '{input}{error}'
+                    ]
+                )->textInput(['placeholder' => 'Ваше имя']); ?>
+
+                <?= $form->field($questionForm, 'email',
+                    [
+                        'horizontalCssClasses' => [
+                            'wrapper' => 'col-sm-2',
+                        ],
+                        'template' => '{input}{error}'
+                    ]
+                )->textInput(['placeholder' => 'E-mail']); ?>
+
+                <?= $form->field($questionForm, 'phone',
+                    [
+                        'horizontalCssClasses' => [
+                            'wrapper' => 'col-sm-2',
+                        ],
+                        'template' => '{input}{error}'
+                    ]
+                )->textInput(['placeholder' => 'Телефонный номер']); ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="form-group">
+                    <?= Html::submitButton('ОТПРАВИТЬ ЗАЯВКУ', ['class' => 'btn btn-primary']) ?>
+                </div>
+                <?php ActiveForm::end() ?>
+            </div>
+        </div>        
+    </div>
+    
+
+</div>
