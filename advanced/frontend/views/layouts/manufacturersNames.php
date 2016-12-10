afbd2e49640fa907cc2f7b496771c32cc06d1219
Antis <antis.smith@gmail.com>
2016-12-10 13:40:51 +0300

    chore(manufacturers) массив выделен в отделный файл
    для последующего подключения на других страницах

---
+++ advanced/frontend/views/layouts/manufacturersNames.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Antis
+ * Date: 10.12.2016
+ * Time: 13:34
+ */
+
+
+$logoNames = [
+    's-01_agoprofil',
+    's-02_astor',
+    's-03_bosca',
+    's-04_casali',
+    's-05_cocif',
+    's-06_colombo',
+    's-07_comas',
+    's-08_cora',
+    's-09_dierre',
+    's-10_foa',
+    's-11_ghizzi-benatti',
+    's-12_henry-glass',
+    's-13_impronta',
+    's-14_itlas',
+    's-15_lineacali',
+    's-16_longhi',
+    's-17_lualdi',
+    's-18_design-porte',
+    's-19_oikos',
+    's-20_paolo-lucchetta',
+    's-21_porte-in-door',
+    's-22_res',
+    's-23_romagnoli',
+];
--- advanced/frontend/views/site/home/block_10-manufacturers.php
+++ advanced/frontend/views/site/home/block_10-manufacturers.php
@@ -10,31 +10,7 @@ use yii\helpers\Html;
 
 /*  assets  */
 
-$logoNames = [
-    's-01_agoprofil',
-    's-02_astor',
-    's-03_bosca',
-    's-04_casali',
-    's-05_cocif',
-    's-06_colombo',
-    's-07_comas',
-    's-08_cora',
-    's-09_dierre',
-    's-10_foa',
-    's-11_ghizzi-benatti',
-    's-12_henry-glass',
-    's-13_impronta',
-    's-14_itlas',
-    's-15_lineacali',
-    's-16_longhi',
-    's-17_lualdi',
-    's-18_design-porte',
-    's-19_oikos',
-    's-20_paolo-lucchetta',
-    's-21_porte-in-door',
-    's-22_res',
-    's-23_romagnoli',
-]
+require_once Yii::getAlias('@frontend').'/views/layouts/manufacturersNames.php';
 ?>
 <article id="manufacturers" class="wrap-manufacturers">
     <div class="row">
