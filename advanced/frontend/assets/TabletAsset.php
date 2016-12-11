1b969f049fcd06d1cb263ae4909c3454ed3a58d2
Andrey <andrey1987.gorbunov@yandex.ru>
2016-12-11 11:18:36 +0300

    new asset

---
+++ advanced/frontend/assets/TabletAsset.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class TabletAsset extends AssetBundle{
+	public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        
+    ];
+}
