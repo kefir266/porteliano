a537d0b33c00c54c3f6cc47ad94e63fc3f9387be
Antis <antis.smith@gmail.com>
2016-12-12 17:39:28 +0300

    fix(block_2) расположение плиток

--- advanced/frontend/views/site/home/block_02-catalog.php
+++ advanced/frontend/views/site/home/block_02-catalog.php
@@ -41,7 +41,7 @@ require_once Yii::getAlias('@frontend').'/views/layouts/manufacturersNames.php';
                         </div>
                     </div>
                 </div>
-                <div class="col-md-3 col-sm-1 col-xs-1">
+                <div class="col-md-3 col-sm-1 col-xs-6">
                     <div class="row">
                         <div class="col-md-12 col-sm-12 col-xs-12">
                             <div class="grips">
@@ -61,7 +61,7 @@ require_once Yii::getAlias('@frontend').'/views/layouts/manufacturersNames.php';
                         </div>
                     </div>
                 </div>
-                <div class="col-md-3 col-sm-4 col-xs-5">
+                <div class="col-md-3 col-sm-4 col-xs-6">
                     <div class="entryDoors">
                         <div class="doors-gradient">
                             <h3>ВХОДНЫЕ ДВЕРИ</h3>
