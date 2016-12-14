5dda0e7f401a89af8680c9cb9f44156a688b9979
Antis <antis.smith@gmail.com>
2016-12-14 13:59:55 +0300

    chore(index.php) спрятал пока не готовые блоки

--- advanced/frontend/assets/TabletAsset.php
+++ advanced/frontend/assets/TabletAsset.php
@@ -7,8 +7,8 @@ class TabletAsset extends AssetBundle{
 	public $basePath = "@webroot";
 	public $baseUrl = "@web";
 	public $css = [
-		//'css/tablet.css',
-		'css/tablet_temp.css',
+		'css/tablet.css',
+		//'css/tablet_temp.css',
 	];
 
 	public $js = [
--- advanced/frontend/views/site/index.php
+++ advanced/frontend/views/site/index.php
@@ -34,7 +34,7 @@ $this->title = 'home';
 
     require_once 'home/block_05-key-benefits.php';
 
-    require 'home/block_06-doors.php';
+    require 'home/block_06-doors.php';/*
 
     require_once 'home/block_07-facts.php';
 
@@ -44,6 +44,6 @@ $this->title = 'home';
     require_once 'home/block_10-manufacturers.php';
     require_once 'home/block_11-about.php';
     require_once 'home/block_12-Contacts.php';
-
+*/
     ?>
 </div>
