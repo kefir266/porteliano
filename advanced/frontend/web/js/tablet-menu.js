1a2ebd9dd22d4c3bd337bcffa10ed1adcc6ebc4c
Andrey <andrey1987.gorbunov@yandex.ru>
2016-12-15 03:01:29 +0300

    tablet(manufacturers)

--- advanced/frontend/web/js/tablet-menu.js
+++ advanced/frontend/web/js/tablet-menu.js
@@ -1,5 +1,6 @@
 $("#button-toggle").click(function(){
-	$('ul#w17').slideToggle(100);
+	//console.log($('ul#w17'));
+	$('ul#w17').slideToggle(200);
 });
 
 $('ul#w17 > li').delegate('a', 'click', function(){
